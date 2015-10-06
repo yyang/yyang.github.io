eaa3e32e0776d8f2f97262f34de72da2a0571a69
Yi Yang <git@iyyang.com>
2015-10-06 17:24:45 +0800

    Initialized with Emerald theme.

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var normal = document.getElementById("nav-menu");
+
+var icon = normal !== null ? normal : reverse;
+
+// Toggle the "menu-open" % "menu-opn-left" classes
+function toggle() {
+	  var nav = document.getElementById("nav");
+
+	  var button = document.getElementById("menu");
+	  var site = document.getElementById("wrap");
+	  
+	  if (nav.className == "menu-open" || nav.className == "menu-open-left") {
+	  	  nav.className = "";
+	  	  button.className = "";
+	  	  site.className = "";
+	  } else {
+	  	  nav.className += "menu-open";
+	  	  button.className += "btn-close";
+	  	  site.className += "fixed";
+	    }
+	}
+
+// Ensures backward compatibility with IE old versions
+function menuClick() {
+	if (document.addEventListener && icon !== null) {
+		icon.addEventListener('click', toggle);
+	} else if (document.attachEvent && icon !== null) {
+		icon.attachEvent('onclick', toggle);
+	} else {
+		return;
+	}
+}
+
+menuClick();
